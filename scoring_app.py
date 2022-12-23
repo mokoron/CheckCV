eb65a5dbf846d2ca122c84687fe2795e67421b7d
mokoron <mokoron@gmail.com>
2022-12-23 17:10:10 +0600

    Update scoring_app.py

--- scoring_app.py
+++ scoring_app.py
@@ -13,7 +13,7 @@ model = Scoring.load_model()
 
 def extract_data(feed):
     data = []
-    with pdfplumber.load(feed) as pdf:
+    with pdfplumber.open(feed) as pdf:
         pages = pdf.pages
         for p in pages:
             data.append(p.extract_tables())
