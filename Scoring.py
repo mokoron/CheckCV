567de770187758225b6104e5bc6c6efe529d02f4
mokoron <mokoron@gmail.com>
2022-12-23 15:32:07 +0600

    Update Scoring.py

--- Scoring.py
+++ Scoring.py
@@ -7,18 +7,21 @@ import streamlit as st
 
 @st.cache
 def load_model():
-   # model = SentenceTransformer('sentence-transformers/all-roberta-large-v1')
-   # model = 'model\pytorch_model.bin'
+    # model = SentenceTransformer('sentence-transformers/all-roberta-large-v1')
+    # model = 'model\pytorch_model.bin'
     model = SentenceTransformer('all-MiniLM-L12-v2')
     return model
 
-def embedding(text, model=load_model()):
+
+def embedding(text, model):
     emb = model.encode(text)
     return emb
 
+
 def similar(emb1, emb2):
     similarity = list(cosine_similarity(emb1.reshape(1, -1), emb2.reshape(1, -1)))
-    return similarity[0]*100
+    return similarity[0] * 100
+
 
 def pdfread(uploaded_doc):
     with fitz.open(uploaded_doc) as doc:
@@ -27,7 +30,7 @@ def pdfread(uploaded_doc):
             cv = ''.join([cv, page.get_text()])
     return cv
 
+
 def docxread(uploaded_doc):
     cv = docx2txt.process(uploaded_doc)
     return cv
-
