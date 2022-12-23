d53a23fe89d0cc100a7791ad382f1703defda00b
mokoron <mokoron@gmail.com>
2022-12-23 14:30:52 +0600

    add files
    add initial project

---
+++ Scoring.py
@@ -0,0 +1,31 @@
+#from sentence_transformers import SentenceTransformer, util
+from sklearn.metrics.pairwise import cosine_similarity
+import docx2txt
+import fitz
+import streamlit as st
+
+@st.cache
+def load_model():
+   # model = SentenceTransformer('sentence-transformers/all-roberta-large-v1')
+   # model = SentenceTransformer('all-MiniLM-L12-v2')
+    model = 'model\pytorch_model.bin'
+    return model
+
+def embedding(text, model=load_model):
+    emb = model.encode(text)
+    return emb
+
+def similar(emb1, emb2):
+    similarity = list(cosine_similarity(emb1.reshape(1, -1), emb2.reshape(1, -1)))
+    return similarity[0]*100
+
+def pdfread(uploaded_doc):
+    with fitz.open(uploaded_doc) as doc:
+        for page in doc:
+            cv = ''.join([cv, page.get_text()])
+    return cv
+
+def docxread(uploaded_doc):
+    cv = docx2txt.process(uploaded_doc)
+    return cv
+
---
+++ scoring_app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import Scoring
+
+
+st.set_page_config(page_title="Check if your CV fits the desirable position", layout="centered", page_icon="📝",)
+st.header('Check if your CV fits the desirable position :sunglasses:')
+
+#st.title("📝 Check if your CV fits the desirable position")
+
+model = Scoring.load_model()
+with st.container():
+    with st.form(key='cv_jobposition'):
+
+        uploaded_file = st.file_uploader("Choose a CV file", type=['pdf', 'docx'])
+        cv_file = "none"
+
+        job_txt = st.text_area('Enter the job description here', height=300, value="Superhero Performer for Birthday Parties and Events. \nMust be energetic and love to work with kids. \nTraining provided. \nMost events include tips and travel. \nMust be have reliable transportation. \nBackground check required. \nExcellent pay. Must be 18+ Most events will be on the weekend during the day time.")
+
+        submitted = st.form_submit_button("Check")
+        if submitted:
+            with st.spinner('Calculating...'):
+                #time.sleep(5)
+                st.write("run script ")
+                if uploaded_file is not None:
+                    if uploaded_file.name[0][-4:] == 'docx':
+                        cv_file = Scoring.docxread(uploaded_file)
+                    else:
+                        cv_file = Scoring.pdfread(uploaded_file)
+                else: st.write("Upload a CV to score")
+                score = Scoring.similar(Scoring.embedding(cv_file), Scoring.embedding(job_txt))[0]
+            st.success('Similarity score is:', score)
+
+st.write(cv_file)
+st.markdown("[mokoron.com](mokoron.com)")
